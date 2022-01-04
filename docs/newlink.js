779ab8125e28bdfbc1054e14fcf5f81ddc106c67
Manga man <72046004+Manga-Man@users.noreply.github.com>
2022-01-04 12:10:07 -0800

    Add files via upload

---
+++ docs/newlink.js
@@ -0,0 +1,142 @@
+var tries = 0;
+var txtfile = ('codes.txt')
+console.log(txtfile)
+if(Notification.permission === 'default') {
+    Notification.requestPermission()
+}
+function sleepFor(sleepDuration){
+    var now = new Date().getTime();
+    while(new Date().getTime() < now + sleepDuration){ /* Do nothing */ }
+}
+
+function getlink() {
+    
+    var code = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+         var print = code[Math.floor(Math.random() * code.length)]
+         var code2 = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+         var print2 = code2[Math.floor(Math.random() * code2.length)]
+         var code3 = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+         var print3 = code3[Math.floor(Math.random() * code3.length)]
+         var code4 = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+         var print4 = code4[Math.floor(Math.random() * code4.length)]
+         var code5 = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+         var print5 = code5[Math.floor(Math.random() * code5.length)]
+         var code6 = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+         var print6 = code6[Math.floor(Math.random() * code6.length)]
+         var code7 = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+         var print7 = code7[Math.floor(Math.random() * code7.length)]
+         var code8 = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+         var print8 = code8[Math.floor(Math.random() * code8.length)]
+         var code9 = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+         var print9 = code9[Math.floor(Math.random() * code9.length)]
+         var code10 = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+         var print10 = code10[Math.floor(Math.random() * code10.length)]
+         var code11 = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+         var print11 = code11[Math.floor(Math.random() * code11.length)]
+         var code12 = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+         var print12 = code12[Math.floor(Math.random() * code12.length)]
+         var code13 = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+         var print13 = code13[Math.floor(Math.random() * code13.length)]
+         var code14 = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+         var print14 = code14[Math.floor(Math.random() * code14.length)]
+         var code15 = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+         var print15 = code15[Math.floor(Math.random() * code15.length)]
+         var code16 = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+         var print16 = code16[Math.floor(Math.random() * code16.length)]
+    
+   function getcode() {
+       document.write("https://discord.gift/")
+       document.write(print)
+       document.write(print2)
+       document.write(print3)
+       document.write(print4)
+       document.write(print5)
+       document.write(print6)
+       document.write(print7)
+       document.write(print8)
+       document.write(print9)
+       document.write(print10)
+       document.write(print11)
+       document.write(print12)
+       document.write(print13)
+       document.write(print14)
+       document.write(print15)
+       document.write(print16)
+       document.write("<br>")
+
+       var therealcode = print + print2 + print3 + print4 + print5 + print6 + print7 + print8 + print9 + print10 + print11 + print12 + print13 + print14 + print15 + print16
+       console.log(therealcode)
+      
+   }
+   
+   
+   
+
+   
+
+   fetch('https://discordapp.com/api/v9/entitlements/gift-codes/' + print + print2 + print3 + print4 + print5 + print6 + print7 + print8 + print9 + print10 + print11 + print12 + print13 + print14 + print15 + print16 + '')
+   
+
+   .then(data => {
+      
+       console.log(data.status) /* logs: 404, 429 or 200*/
+       console.log(data.ok) /* logs true or false...*/
+       console.log(tries)
+       sleepFor(1000)
+       tries++
+
+       if(tries === 5) {
+           console.log('sleeping for 60 seconds....')
+           sleepFor(60000)
+           location.reload()
+       } else{
+       if(data.status === 429) {
+         console.log('429: rate limited')
+           tries++
+           
+
+       } else if(data.status === 200) {
+           console.log('valid code!!!')
+           
+           
+           sleepFor(9000000000000000000000000)
+           
+       }
+    }
+   }
+   )
+   
+
+   getcode()
+   
+   }
+
+
+
+getlink()
+
+
+getlink()
+
+
+getlink()
+
+
+getlink()
+
+
+getlink()
+
+
+getlink()
+
+
+getlink()
+
+
+
+ console.error('uhh something is off...')
+
+
+ 
+   
